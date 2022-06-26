2a8bfb62abfeaa9a6b667b8ac539184a0ea6e50c
Adrian Navarro Gabino <adriannavarrogabino@gmail.com>
2022-06-26 22:41:07 +0200

    Image and picker creation

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PickerController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,50 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  public timer: number = 25;
 
-  constructor() {}
+  constructor(private pickerCtrl: PickerController) {}
 
+  pomodoro() {
+    alert("hola");
+  }
+
+
+  async openPicker() {
+    const picker = await this.pickerCtrl.create({
+      columns: [
+        {
+          name: 'timer',
+          options: [
+            {
+              text: '25 minutes',
+              value: '25',
+            },
+            {
+              text: '15 minutes',
+              value: '15',
+            },
+            {
+              text: '5 minutes',
+              value: '5',
+            }
+          ]
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+        },
+        {
+          text: 'Confirm',
+          handler: (value) => {
+            this.timer = value.timer.value;
+          },
+        },
+      ],
+    });
+
+    await picker.present();
+  }
 }
